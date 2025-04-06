43f07e47dcdc5e2e39f314b7ef4ae80ff58e7bbb
Kyalo <jkyalo@seaburysolutions.com>
2025-04-06 03:23:53 +0300

    InvoiceUpdateDto.cs

---
+++ InvoiceManagementSystemAPI/Models/Dto/InvoiceUpdateDto.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceManagementSystemAPI.Models.Dto;
+
+public class InvoiceUpdateDto
+{              [Required]
+               public int InvoiceId { get; set; }
+               [Required]
+               public string CustomerAccountNumber { get; set; }
+               [Required]
+               public string Item {  get; set; }
+               [Required]
+               public  DateTime  InvoiceDate { get; set; }
+               [Required]
+               public string VehicleRegistration { get; set; }
+               public  string Description { get; set; }
+               [Required]
+               public string CollectionSlipNumber { get; set; }
+               [Required]
+               public string Status { get; set; }
+               [Required]
+               public decimal VAT {  get; set; }
+               [Required]
+               public decimal Rate { get; set; }
+               [Required]
+               public int Quantity { get; set; }
+               [Required]
+               public decimal Total { get; set; }
+}
