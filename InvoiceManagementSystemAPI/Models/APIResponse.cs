94da60c224537f6f8f3b47f2358567a2f70081f1
Kyalo <jkyalo@seaburysolutions.com>
2025-04-06 03:06:59 +0300

    Standard API Response

---
+++ InvoiceManagementSystemAPI/Models/APIResponse.cs
@@ -0,0 +1,16 @@
+using System.Net;
+
+namespace InvoiceManagementSystemAPI.Models;
+
+public class APIResponse
+{
+
+   
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> Errors { get; set; }
+        public object Result { get; set; }
+
+
+
+}
