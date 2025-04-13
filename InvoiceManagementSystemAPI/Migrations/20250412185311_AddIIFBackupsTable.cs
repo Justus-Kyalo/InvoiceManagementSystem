4470fbfbf8d8d57da001ea17125dfbf8aec5c197
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-04-13 03:06:28 +0300

    migration

---
+++ InvoiceManagementSystemAPI/Migrations/20250412185311_AddIIFBackupsTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceManagementSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIIFBackupsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IIFBackups",
+                columns: table => new
+                {
+                    IIFBackupId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GeneratedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IIFBackups", x => x.IIFBackupId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IIFBackups");
+        }
+    }
+}
