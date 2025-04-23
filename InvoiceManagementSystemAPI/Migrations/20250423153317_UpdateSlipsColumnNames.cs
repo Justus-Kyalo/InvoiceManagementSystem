5bf4b4f90bcafc020f2263acafbf41ffb343ab85
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-04-23 18:54:41 +0300

    UpdateColumnNames

---
+++ InvoiceManagementSystemAPI/Migrations/20250423153317_UpdateSlipsColumnNames.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceManagementSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSlipsColumnNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "updatedDate",
+                table: "Slips",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "createdDate",
+                table: "Slips",
+                newName: "CreatedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Slips",
+                newName: "updatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Slips",
+                newName: "createdDate");
+        }
+    }
+}
