9e4ccdd63c709bdc5c329b37e6785f67906ec767
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-05 10:21:50 +0300

    Customer Extend CustomerItemPrices

--- InvoiceManagementSystemAPI/Models/Customer.cs
+++ InvoiceManagementSystemAPI/Models/Customer.cs
@@ -8,9 +8,13 @@ public class Customer
     [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int CustomerId { get; set; }
     [Required]
-    public  string AccountNumber { get; set; }
+    public  int AccountNumber { get; set; }
     [Required]
     public string Name { get; set; }
     [Required]
     public  bool Taxable { get; set; }
+    [Required]
+    public bool Active { get; set; }
+    [Required]
+    public ICollection<CustomerItemPrice> CustomerItemPrices { get; set; }
 }
