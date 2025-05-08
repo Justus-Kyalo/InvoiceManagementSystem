883ea54b2a4b5e0c8ca5413a0f99cdbd82007ad6
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-08 11:22:00 +0300

    CustomerItemPriceUpdateDto

--- InvoiceManagementSystemAPI/Models/Dto/CustomerItemPriceUpdateDto.cs
+++ InvoiceManagementSystemAPI/Models/Dto/CustomerItemPriceUpdateDto.cs
@@ -1,12 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InvoiceManagementSystemAPI.Models.Dto;
 
 public class CustomerItemPriceUpdateDto
 {
+    [Required]
     public int CustomerItemPriceId { get; set; }
-  
+    [Required]
     public int CustomerId { get; set; }
-    
+    [Required]
     public int ItemId { get; set; }
-    
+    [Required]
     public decimal Price { get; set; }
 }
