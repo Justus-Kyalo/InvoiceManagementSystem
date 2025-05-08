7f42f8072c90af57e4fa4159d915f0c5539ef928
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-08 11:21:15 +0300

    CustomerItemPriceCreateDto

--- InvoiceManagementSystemAPI/Models/Dto/CustomerItemPriceCreateDto.cs
+++ InvoiceManagementSystemAPI/Models/Dto/CustomerItemPriceCreateDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InvoiceManagementSystemAPI.Models.Dto;
 
 public class CustomerItemPriceCreateDto
 {
-    
+    [Required]
     public int ItemId { get; set; }
-    
+    [Required]
     public decimal Price { get; set; }
 }
