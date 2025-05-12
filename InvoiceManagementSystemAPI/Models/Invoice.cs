39c6e991cb7629b14778964db7da87beecbb2247
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-12 18:36:26 +0300

    Invoice Model

---
+++ InvoiceManagementSystemAPI/Models/Invoice.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InvoiceManagementSystemAPI.Models;
+
+public class Invoice
+{
+    [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int InvoiceId { get; set; }
+    [Required]
+    [ForeignKey("Customer")]
+    public int CustomerId { get; set; }
+    [Required]
+    public  DateTime TaxDate { get; set; }
+    [Required]
+    public  int JobNumber { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndDate { get; set; }
+    public Customer?  Customer { get; set; }
+}
