de71ddde0358b9d52146ea0b4a32dcd455f47215
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-17 14:56:49 +0300

    CustomerName

--- InvoiceManagementSystemAPI/Models/Dto/InvoiceDto.cs
+++ InvoiceManagementSystemAPI/Models/Dto/InvoiceDto.cs
@@ -3,7 +3,7 @@ namespace InvoiceManagementSystemAPI.Models.Dto;
 public class InvoiceDto
 {
     public int InvoiceId { get; set; }
-    public int CustomerId { get; set; }
+    public string CustomerName { get; set; }
     public  DateTime TaxDate { get; set; }
     public  int JobNumber { get; set; }
     public DateTime StartDate { get; set; }
