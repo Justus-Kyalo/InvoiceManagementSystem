e9f0a5d754395f63bc5b258ebbc4ff4ebd794ed7
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-19 11:37:10 +0300

    CreatedAt & UpdatedAt

---
+++ InvoiceManagementSystemAPI/Migrations/20250519083533_UpdateAtCreatedAt.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceManagementSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAtCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Vehicles",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Vehicles",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Slips",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Slips",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "SlipItems",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "SlipItems",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Invoices",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Invoices",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Customers",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Customers",
+                newName: "CreatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "CustomerItemPrices",
+                newName: "UpdatedAt");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "CustomerItemPrices",
+                newName: "CreatedAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Vehicles",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Vehicles",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Slips",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Slips",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "SlipItems",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "SlipItems",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Invoices",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Invoices",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Customers",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Customers",
+                newName: "CreatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "CustomerItemPrices",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "CustomerItemPrices",
+                newName: "CreatedDate");
+        }
+    }
+}
