601053f06c905463a0f4b75d55a871e3dfcbd986
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-19 11:44:53 +0300

    BackUp Db

---
+++ InvoiceManagementSystemAPI/Migrations/20250519084116_CreatedAt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceManagementSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
