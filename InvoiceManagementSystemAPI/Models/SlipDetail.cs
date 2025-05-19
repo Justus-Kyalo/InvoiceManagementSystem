23cbb3772577fa005bad686f3fede1b9a0398cd6
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-19 13:07:35 +0300

    ALTER VIEW 'vw_SlipDetails'

--- InvoiceManagementSystemAPI/Models/SlipDetail.cs
+++ InvoiceManagementSystemAPI/Models/SlipDetail.cs
@@ -15,5 +15,7 @@ public class SlipDetail
     public string ItemName { get; set; }
     public int Quantity { get; set; }
     public decimal Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 
 }
