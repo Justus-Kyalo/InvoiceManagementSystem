aa5412a1eb7facd61d67e4ee4b9f7ff4581f569d
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-05-31 13:05:50 +0300

    Migrations

---
+++ InvoiceManagementSystemAPI/Migrations/20250530123553_NullableVehicleIdFK.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceManagementSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableVehicleIdFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Slips_Vehicles_VehicleId",
+                table: "Slips");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "SlipItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "SlipItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "Slips",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Slips_Vehicles_VehicleId",
+                table: "Slips",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "VehicleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Slips_Vehicles_VehicleId",
+                table: "Slips");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "Slips",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "SlipItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "SlipItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Slips_Vehicles_VehicleId",
+                table: "Slips",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "VehicleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
