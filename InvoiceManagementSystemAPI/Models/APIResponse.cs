8f0533c4234fc2a10f3d49222b7dba58563e4064
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-06-16 18:08:50 +0300

    APIResponse Errors List instantiation

--- InvoiceManagementSystemAPI/Models/APIResponse.cs
+++ InvoiceManagementSystemAPI/Models/APIResponse.cs
@@ -4,13 +4,13 @@ namespace InvoiceManagementSystemAPI.Models;
 
 public class APIResponse
 {
+    public APIResponse()
+    {
+        Errors = new List<string>();
+    }
 
-   
-        public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; } = true;
-        public List<string> Errors { get; set; }
-        public object Result { get; set; }
-
-
-
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public List<string> Errors { get; set; }
+    public object Result { get; set; }
 }
