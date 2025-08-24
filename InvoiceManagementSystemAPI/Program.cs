ef87fa595040cede9302cf547c2195305dd0ac24
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-08-24 14:32:34 +0300

    IMS refactoring

--- InvoiceManagementSystemAPI/Program.cs
+++ InvoiceManagementSystemAPI/Program.cs
@@ -65,9 +65,6 @@ builder.Services.AddAuthentication(x =>
         ValidateAudience = false
     };
 });
-
-
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
