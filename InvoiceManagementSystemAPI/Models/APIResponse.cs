e95e17f5c7ec39185d5f30224e2660152d6d0761
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-08-25 08:01:00 +0300

    Automate success status APIResponse

--- InvoiceManagementSystemAPI/Models/APIResponse.cs
+++ InvoiceManagementSystemAPI/Models/APIResponse.cs
@@ -10,7 +10,7 @@ public class APIResponse
     }
 
     public HttpStatusCode StatusCode { get; set; }
-    public bool IsSuccess { get; set; }
+    public bool IsSuccess => ((int)StatusCode >= 200 && (int)StatusCode <= 300);
     public List<string> Errors { get; set; }
     public object Result { get; set; }
 }
