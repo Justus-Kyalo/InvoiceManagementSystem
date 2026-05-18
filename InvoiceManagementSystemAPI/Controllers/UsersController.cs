44610ddf9b6d23cc36c5935a6814b507746ad98a
Justus Kyalo <jkyalo@seaburysolutions.com>
2026-05-18 12:28:48 +0300

    Add Issuer and Audience

--- InvoiceManagementSystemAPI/Controllers/UsersController.cs
+++ InvoiceManagementSystemAPI/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using FluentValidation;
+using FluentValidation.Results;
 using InvoiceManagementSystemAPI.Models;
 using InvoiceManagementSystemAPI.Models.Dto;
 using InvoiceManagementSystemAPI.Repository.IRepository;
@@ -7,59 +9,97 @@ using Microsoft.AspNetCore.Mvc;
 namespace InvoiceManagementSystemAPI.Controllers;
 [Route("api/UsersAuth")]
 [ApiController]
-public class UsersController:ControllerBase
+public class UsersController : ControllerBase
 {
     protected APIResponse _response;
     private readonly IUserRepository _userRepo;
-    public UsersController(IUserRepository userRepo)
+    private readonly IValidator<RegistrationRequestDTO> _registrationValidator;
+
+    public UsersController(
+        IUserRepository userRepo,
+        IValidator<RegistrationRequestDTO> registrationValidator)
     {
         _userRepo = userRepo;
-        this._response = new();
+        _registrationValidator = registrationValidator;
+        _response = new();
     }
 
     [HttpPost("login")]
     [ProducesResponseType(200)]
     [ProducesResponseType(400)]
     [ProducesResponseType(500)]
-    public async Task<IActionResult> Login([FromBody] LoginRequestDTO model)
+    public async Task<IActionResult> Login(
+        [FromBody] LoginRequestDTO model)
     {
         var loginResponse = await _userRepo.Login(model);
-        if (loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token))
+
+        if (loginResponse.User == null ||
+            string.IsNullOrEmpty(loginResponse.Token))
         {
             _response.StatusCode = HttpStatusCode.BadRequest;
             _response.Errors.Add("Username or Password is Incorrect");
+
             return BadRequest(_response);
         }
 
         _response.StatusCode = HttpStatusCode.OK;
         _response.Result = loginResponse;
-        return Ok(_response);
 
+        return Ok(_response);
     }
 
     [HttpPost("register")]
     [ProducesResponseType(200)]
     [ProducesResponseType(400)]
     [ProducesResponseType(500)]
-    public async Task<IActionResult> Register([FromBody] RegistrationRequestDTO model)
+    public async Task<IActionResult> Register(
+        [FromBody] RegistrationRequestDTO model)
     {
-        bool ifUserNameUnique = _userRepo.IsUniqueUser(model.UserName);
-        if (!ifUserNameUnique)
+        ValidationResult validationResult =
+            await _registrationValidator.ValidateAsync(model);
+
+        if (!validationResult.IsValid)
         {
             _response.StatusCode = HttpStatusCode.BadRequest;
-            _response.Errors.Add("User with the username already exists ");
-            return BadRequest(_response);
+
+            foreach (var error in validationResult.Errors)
+            {
+                _response.Errors.Add(error.ErrorMessage);
             }
 
+            return BadRequest(_response);
+        }
+
+        bool isUserNameUnique =
+            _userRepo.IsUniqueUser(model.UserName);
+
+        if (!isUserNameUnique)
+        {
+            _response.StatusCode = HttpStatusCode.BadRequest;
+
+            _response.Errors.Add(
+                "User with the username already exists.");
+
+            return BadRequest(_response);
+        }
+
+        
+        model.Role = "clerk";
+
         var user = await _userRepo.Register(model);
+
         if (user == null)
         {
             _response.StatusCode = HttpStatusCode.BadRequest;
+
             _response.Errors.Add("Error while Registering");
+
             return BadRequest(_response);
         }
 
         _response.StatusCode = HttpStatusCode.OK;
+        _response.Result = user;
+
         return Ok(_response);
     }
 }
